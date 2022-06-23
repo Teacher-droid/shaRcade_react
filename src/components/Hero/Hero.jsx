c99a589f4dfbae95e062bdebe53138ff12840762
Teacher-droid <teaching.know@gmail.com>
2022-06-23 12:08:29 +0200

    adding app.css & hero.css imports, adding section tag and specific classname to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../../App.css';
+import './Hero.css';
+
+
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section className='hero-container'>
+            <h1>Just shaRcade it</h1>
+            <p>The best of the beast</p>
+            <div className='hero-slide'>
+            </div>
+        </section>
     )
 }
 
