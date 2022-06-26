fe87d282cab52cd6b6bd8309684a05a9c594e7a1
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-26 20:24:53 +0200

    create const for GameShow and add comments to localize GaleShow part in GameCard

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -15,8 +15,30 @@ function GameCard(props) {
     // Returns ⭐ to ⭐⭐⭐⭐⭐ depending on current user evaluation of "props.game"
   }
 
+// BEGIN "GameShow" ==> Instead of going to the game page, displaying game details over (z index) game index.
+
   const [viewMore, setViewMore] = useState(false);
   const linkName = viewMore ? 'View Less << ' : '...View More >> '
+
+  const showDetails = <div class="modal-bg" onClick={()=>{setViewMore(!viewMore)}}>
+      <div className="game-card modal">
+      <div className="game-card-header">
+      <div className="game-favorite">{gameFavoriteIcon}</div>
+      <div className="game-feedback">{gameFeedbackIcons}</div>
+      </div>
+      <div className="game-card-body">
+      <h3>{props.game.game_title}</h3>
+      <p>{props.game.game_descr}<span className="show-button" onClick={()=>{setViewMore(!viewMore)}}>{linkName}</span></p>
+      </div>
+      <hr />
+      <div className="game-card-footer">
+      </div>
+      <div className="game-fan">***[Insérer ici le nombre de fans du jeu suivi d'un :heart:]**</div>
+      <div className="game-evaluator">**[Insérer la note sous forme d' :star:]**</div>
+      </div>
+      </div>
+
+      // END "GameShow"
 
   return (
     <div className="game-card">
@@ -33,24 +55,7 @@ function GameCard(props) {
     </div>
     <div className="game-fan">***[Insérer ici le nombre de fans du jeu suivi d'un :heart:]**</div>
     <div className="game-evaluator">**[Insérer la note sous forme d' :star:]**</div>
-    {viewMore &&
-      <div class="modal-bg" onClick={()=>{setViewMore(!viewMore)}}>{linkName}>
-      <div className="game-card modal">
-      <div className="game-card-header">
-      <div className="game-favorite">{gameFavoriteIcon}</div>
-      <div className="game-feedback">{gameFeedbackIcons}</div>
-      </div>
-      <div className="game-card-body">
-      <h3>{props.game.game_title}</h3>
-      <p>{props.game.game_descr.slice(0,49)}<span className="show-button" onClick={()=>{setViewMore(!viewMore)}}>{linkName}</span></p>
-      </div>
-      <hr />
-      <div className="game-card-footer">
-      </div>
-      <div className="game-fan">***[Insérer ici le nombre de fans du jeu suivi d'un :heart:]**</div>
-      <div className="game-evaluator">**[Insérer la note sous forme d' :star:]**</div>
-      </div>
-      </div>}
+    {viewMore && showDetails}
       </div>
       );
 }
