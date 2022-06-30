8786d8f3f97c1fdbce65ca63d5b5e368c684835b
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-06-30 21:17:20 +0200

    Ultimate modifications on Logout to manage sessions management

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -10,36 +10,31 @@ const Logout = () => {
 
     let redirectLogOut = useNavigate();
     const [myAuthorization, setAuthorization] = useAtom(authorizationAtom);
-    const [myToken, setMyToken] = useState("");
     const [myUserSession, setUserSession] = useAtom(userAtom);
-    const [myUser, setMyUser] = useState();
 
     // Verifying User existence
     useEffect(() => {
+        console.log("Verifying user effective connection...");
+        console.log(Cookies.get("fulluser"));
         if (Cookies.get("fulluser")) {
-            // console.log("Before '%20' cleaning");
-            // console.log(myToken);
-            // console.log(myAuthorization);
-            setMyToken(Cookies.get("token").replace(/%20/g, " "));
-            setAuthorization(myAuthorization.replace(/%20/g, " "));
-            // console.log("After '%20' cleaning");
-            // console.log(myToken);
-            // console.log(myAuthorization)
-            if (myToken !== myAuthorization) {
-                console.log("Corrupted token(s) - Cannot log you out properly - Bearer token stored in 'fulluser' cookie is different from Jotai authorizationAtom.");
+            console.log("Existing 'fulluser' cookie - Proceeding further to logout.");
+            console.log(myAuthorization)
+            if (myAuthorization) {
+                console.log("Existing token - Proceeding further to logout.");
             } else {
-                console.log("Corresponding token - Proceeding further to logout.");
+                console.log("Corrupted or missing token(s) - Cannot log you out properly.");
             }
         } else {
-            console.log("You're not even connected... Come on!");
+            console.log("Corrupted or missing Cookie - Seems you're not even connected... Come on why trying to logout then ?!");
             redirectLogOut('/', { replace: true }); /* Replacing the "/admindashboard" entry to avoid use of browser "Back" button */
         }
     },[]);
 
     // Attempting disconnection with Bearer token
     useEffect(() => {
-        // console.log(myToken);
-        if (myToken === myAuthorization) {
+        console.log("Sending logout request with bearer hereby:");
+        console.log(myAuthorization);
+        if (myAuthorization) {
             fetch(API_URL + 'users/sign_out', {
                 method: 'delete',
                 headers: {
@@ -48,23 +43,25 @@ const Logout = () => {
                 }
             })
             .then((response) => {
-                // console.log(response);
+                console.log(response);
                 if (response.status===200) {
+                    console.log("Server responded OK (200) to our logout request: launching cookie and atoms deletion.");
                     Cookies.remove("fulluser");
                     setUserSession(null);
                     setAuthorization(null);
                     Cookies.remove("id");
                     Cookies.remove("token");
+                    console.log("All connection beans destroyed. You're logged out successfully! See you soon.");
                     redirectLogOut('/');
                 } else {
-                    console.log("Server response to logout authorization differes from OK (200): cannot disconnect you properly ;-)");
+                    console.log("Server response to logout request differs from OK (200): cannot disconnect you properly ;-)");
                 }
             })
             .catch((error) => console.log(error));
         } else {
-            console.log("Corrupted token(s) - Cannot log you out properly - Bearer token stored in 'fulluser' cookie is different from Jotai authorizationAtom.");
+            console.log("Corrupted or missing token(s) - Cannot log you out properly.");
         }
-    },[myToken, myAuthorization]);
+    },[myAuthorization]);
 
     return (
         <div>Logout</div>
