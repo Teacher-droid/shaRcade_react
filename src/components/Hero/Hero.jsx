ea321d7039bb91ee7207dc3fb413aacd0a87728e
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-02 21:04:52 +0200

    fix text and add game links

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,8 @@ const Hero = () => {
     return (
         <section className='hero-container retro'>
             <h1 className="App">Just shaRcade it!</h1>
-            <p className="App">For now, only snaKouninef is playable and linked to shaRcade. Try it out !<small>Tetrois is playable too, not shaRcade ready though.</small></p>
+            <p className="App">For now, only <a href="https://loiskouninef.github.io/Snake_JSVanilla/"><strong>snaKouninef</strong></a> is playable and linked to shaRcade. Try it out !</p>
+            <p className="App"><small><a href="https://loiskouninef.github.io/Tetris_React">Tetrois</a> is playable too, not shaRcade ready though.</small></p>
         </section>
     )
 }
